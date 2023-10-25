2d28b31e35adce9500a48305fc45691718a7f556
Axel Kaotic <contact@kaotic.fr>
2023-10-25 15:34:50 +0200

    add week number to user recipe list

--- backend/recipes/serializers/recipes_list.py
+++ backend/recipes/serializers/recipes_list.py
@@ -3,8 +3,14 @@ from ..models import RecipesList
 from .recipe import RecipeSerializer
 
 
+class DateTimeToWeekIntegerField(serializers.IntegerField):
+    def to_representation(self, value):
+        return int(value.strftime("%W"))
+
+
 class RecipesListSerializer(serializers.ModelSerializer):
     recipes = RecipeSerializer(many=True, read_only=True)
+    date_week = DateTimeToWeekIntegerField(read_only=True, source='date_created')
 
     class Meta:
         model = RecipesList
