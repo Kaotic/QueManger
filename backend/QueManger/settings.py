b757952698a7a8e9b9aaafdfd2852a7a27226ced
Axel Kaotic <contact@kaotic.fr>
2023-10-26 00:39:06 +0200

    Backend: Change JWT expiration

--- backend/QueManger/settings.py
+++ backend/QueManger/settings.py
@@ -59,8 +59,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=31),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
